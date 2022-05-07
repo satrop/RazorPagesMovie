5c8ced85fcf8ac8ccf62c643da2eae60554b6959
satrop <stevenportas@me.com>
2022-05-06 17:10:06 -0700

    Initial Push

---
+++ Data/RazorPagesMovieContext.cs
@@ -0,0 +1,17 @@
+﻿#nullable disable
+using Microsoft.EntityFrameworkCore;
+
+namespace RazorPagesMovie.Data
+{
+#pragma warning disable CS8632
+    public class RazorPagesMovieContext : DbContext
+    {
+#pragma warning disable CS8632
+        public RazorPagesMovieContext (DbContextOptions<RazorPagesMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovie.Models.Movie>? Movie { get; set; }
+    }
+}
